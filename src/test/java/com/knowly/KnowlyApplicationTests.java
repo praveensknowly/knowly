93f3e5670d365f963fdaadebc0671c16f06c0488
Praveen <praveensknowly@gmail.com>
2026-07-16 15:40:34 +0530

    403 fix

--- src/test/java/com/knowly/KnowlyApplicationTests.java
+++ src/test/java/com/knowly/KnowlyApplicationTests.java
@@ -2,8 +2,15 @@ package com.knowly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.security.oauth2.client.registration.google.client-id=test-id",
+    "spring.security.oauth2.client.registration.google.client-secret=test-secret",
+    "spring.security.oauth2.client.registration.github.client-id=test-id",
+    "spring.security.oauth2.client.registration.github.client-secret=test-secret"
+})
 class KnowlyApplicationTests {
 
 	@Test
